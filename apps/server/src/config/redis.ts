284a854063ce5b1e6be2d8eaf2f285ab6f4bb957
BhupeshB7 <bhupeshkr2912@gmail.com>
2026-07-08 00:37:17 +0530

    chore: setup railway deploy config and upstash redis

--- apps/server/src/config/redis.ts
+++ apps/server/src/config/redis.ts
@@ -1,6 +1,10 @@
 import Redis from 'ioredis'
 import { env } from './env'
 
+const isTLS = env.REDIS_URL.startsWith('rediss://')
+
 export const redis = new Redis(env.REDIS_URL, {
-  maxRetriesPerRequest: null
+  maxRetriesPerRequest: null,
+  family: 0, // Help with IPv6 resolution for Upstash
+  ...(isTLS ? { tls: { rejectUnauthorized: false } } : {})
 })
